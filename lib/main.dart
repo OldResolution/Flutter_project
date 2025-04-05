9cc07899fd2da6c5181b932f0c3fa349e5db6908
OldResolution <140333635+OldResolution@users.noreply.github.com>
2025-04-05 23:52:05 +0530

    major commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'screens/home_screen.dart';
-import 'task_provider.dart';
+import 'package:task_pomodoro_app/screen/home.dart';
 
 void main() {
-  runApp(
-    ChangeNotifierProvider(
-      create: (_) => TaskProvider(),
-      child: MyApp(),
-    ),
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Task Pomodoro',
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
-        fontFamily: 'Roboto',
-      ),
-      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+      home: Home_Screen(),
     );
   }
 }
